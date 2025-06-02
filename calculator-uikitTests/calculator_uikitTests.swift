507125923de0109ef7af87587e026b61898218da
Deepan-G <deepangnanavel@gmail.com>
2025-06-02 20:50:28 +0530

    Initial Commit

---
+++ calculator-uikitTests/calculator_uikitTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_uikitTests.swift
+//  calculator-uikitTests
+//
+//  Created by Deepan Gnanavel on 02/06/25.
+//
+
+import Testing
+@testable import calculator_uikit
+
+struct calculator_uikitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
